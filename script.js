0f8b8f6dd7d87eb8bb39d984eeef8d2d4766ac5e
bokorni <lucasbokorni@gmail.com>
2023-11-22 08:52:58 -0300

    js

---
+++ script.js
@@ -0,0 +1,9 @@
+function clq1 () {
+    alert(`Você clicou no botão 1`)
+}
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", clq2);
+
+function clq2 () {
+    alert(`Você clicou no botão 2`)
+}
